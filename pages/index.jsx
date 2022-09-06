53435d67b77a8375f3da90c29fab67bb7fb070da
Neemias Santos <neemiassgc@outlook.com>
2022-09-06 03:58:26 +0100

    Add a component named SearchBar and make small adjustments

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,13 +1,19 @@
-import Head from 'next/head'
-import Image from 'next/image'
 import { Component } from "react"
-import Table from "../components/table"
 import { BsSearch } from "react-icons/bs"
 
 function Header() {
   return (
     <header className="w-full h-8 bg-blue-600 flex items-center justify-center">
     </header>
+  )
+}
+
+function SearchBar() {
+  return (
+    <div className="h-10 flex border my-3 rounded-lg">
+      <BsSearch className="p-2 text-3xl text-dark bg-gray-100 h-10 w-10"/>
+      <input className="h-full outline-none p-2 text-dark flex-grow" placeholder="Search by keyword"/>
+    </div>
   )
 }
 
@@ -20,8 +26,9 @@ export default class App extends Component {
     return(
       <>
         <Header/>
-        <div className="mt-32">
+        <div className="mt-16">
           <div className="w-fit mx-auto bg-gray-50 p-3 rounded-md shadow-md">
+            <SearchBar/>
           </div>
         </div>
       </>
