704e76933ad22a953620dbfe7492949144543a10
Neemias Santos <neemiassgc@outlook.com>
2022-09-06 19:14:09 +0100

    Implement and export an async function called getPricesByLink

--- components/net.jsx
+++ components/net.jsx
@@ -3,3 +3,7 @@ const URL = "http://localhost:8080/api/products"
 export async function getProducts() {
   return (await fetch(URL)).json()
 }
+
+export async function getPricesByLink(link) {
+  return (await fetch(`${link}&limit=2`)).json()
+}
