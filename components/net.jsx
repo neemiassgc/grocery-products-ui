75f35bc5e1dc99f52f22ea49f6250796745c47f6
Neemias Santos <neemiassgc@outlook.com>
2022-09-15 13:39:39 +0100

    Create an async function named cookData

--- components/net.jsx
+++ components/net.jsx
@@ -15,4 +15,30 @@ export async function getPagedProducts(page, numberOfElements) {
 async function fetchPagedProducts({ page, pageSize }) {
   return (await fetch(`${URL}?pag=${page}-${pageSize}`)).json()
 }
+
+async function cookData(rawData) {
+  const productList = rawData._embedded.List;
+  const productListToReturn = [];
+
+  for (const product of productList) {
+    const linkToFetchPrices = product._links.prices.href;
+    const { description, sequenceCode, barcode } = product
+    const priceList = await net.getPricesByLink(linkToFetchPrices);
+    const { currentPrice, currentPriceDate } = priceList[0];
+    const { previousPrice = null, previousPriceDate = null} = priceList[1];
+
+    productListToReturn.push({
+      description,
+      sequenceCode,
+      barcode,
+      currentPrice,
+      currentPriceDate: new Date(currentPriceDate),
+      previousPrice: previousPrice ?? 0,
+      previousPriceDate: previousPriceDate ? new Date(previousPriceDate) : null,
+      priceDifference: previousPrice ? (previousPrice - currentPrice).toFixed(2) : null
+    })
+  }
+
+  return productListToReturn;
+}
 }
