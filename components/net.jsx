a461e2a98bed98a1eedea904985d85b8de9d3094
Neemias Santos <neemiassgc@outlook.com>
2022-09-16 13:24:31 +0100

    Raname cookData function to cookProducts and extract from it a function named cookPrices

--- components/net.jsx
+++ components/net.jsx
@@ -8,16 +8,27 @@ async function fetchPagedProducts({ page, pageSize }) {
   return (await fetch(`${URL}?pag=${page}-${pageSize}`)).json()
 }
 
-async function cookData(rawData) {
-  const productList = rawData._embedded.List;
+function cookPrices(rawPrices) {
+  return {
+    currentPrice: rawPrices[0].value,
+    currentPriceDate: rawPrices[0].instant,
+    previousPrice: rawPrices[1]?.value,
+    previousPriceDate: rawPrices[1]?.instant,
+  }
+}
+
+async function cookProducts(rawProducts) {
+  const productList = rawProducts._embedded.List;
   const productListToReturn = [];
 
   for (const product of productList) {
     const linkToFetchPrices = product._links.prices.href;
     const { description, sequenceCode, barcode } = product
-    const priceList = await net.getPricesByLink(linkToFetchPrices);
-    const { currentPrice, currentPriceDate } = priceList[0];
-    const { previousPrice = null, previousPriceDate = null} = priceList[1];
+    const priceList = cookPrices(await fetchPricesByLink(linkToFetchPrices));
+    const {
+      currentPrice, currentPriceDate,
+      previousPrice, previousPriceDate,
+    } = priceList;
 
     productListToReturn.push({
       description,
