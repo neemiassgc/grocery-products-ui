9c00b5bb82ea95ea2d68dd84b4f0937b84c2b99e
Neemias Santos <neemiassgc@outlook.com>
2022-09-17 13:04:22 +0100

    Refactor formatPrice method

--- components/utils.jsx
+++ components/utils.jsx
@@ -2,13 +2,11 @@ function formatDate(date) {
   return `${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`;
 }
 
-export function formatPrice(price) {
-  const sign = price < 0 ? "-" : "+";
-  price *= 1;
-
+function formatPrice(price) {
   let [integerPart, decimalPart] = (""+price).split(".");
   if (!decimalPart) decimalPart = "00"
   else if (decimalPart.length === 1) decimalPart += 0
 
-  return `${sign} R$ ${integerPart},${decimalPart}`;
+  return `R$ ${integerPart},${decimalPart}`;
+}
 }
