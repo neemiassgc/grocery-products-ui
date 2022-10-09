b0c93881e33361f3770e80cfe059609b7e688274
Neemias Santos <neemiassgc@outlook.com>
2022-10-09 03:38:49 +0100

    Create a sketch of the component DialogView

--- components/viewmodal.jsx
+++ components/viewmodal.jsx
@@ -1,5 +1,13 @@
 import { Component } from "react"
-import { Dialog, DialogContent, DialogTitle, DialogActions, Box, Typography, Button } from "@mui/material"
+import { Dialog, DialogContent, DialogTitle, DialogActions, Button, Alert, AlertTitle } from "@mui/material"
+import List from "@mui/material/List"
+import ListItem from "@mui/material/ListItem"
+import ListItemText from "@mui/material/ListItemText"
+import ListItemIcon from "@mui/material/ListItemIcon"
+import { MdDescription } from "react-icons/md"
+import { AiOutlineBarcode } from "react-icons/ai"
+import { SiCoderwall } from "react-icons/si"
+import { GiPriceTag } from "react-icons/gi"
 
 class DialogView extends Component {
 
@@ -7,25 +15,45 @@ class DialogView extends Component {
     super(props)
   }
 
-  handleOpen() {
-    this.props.open = true;
-  }
-
-  handleClose() {
-    this.props.open = false;
-  }
-
   render() {
     return (
       <Dialog
-        onClose={this.handleClose.bind(this)}
+        fullWidth={true}
+        maxWidth={"sm"}
         open={this.props.open}
       >
         <DialogTitle>
-          <Button onClick={this.handleClose.bind(this)}>Close</Button>
+          <Alert severity="success" variant="filled">
+            <AlertTitle>Product is already exist!</AlertTitle>
+          </Alert>
         </DialogTitle>
-        <DialogContent>This is the content</DialogContent>
-        <DialogActions>Here should stay the actions</DialogActions>
+        <DialogContent dividers={true}>
+          <List>
+            <ListItem className="flex gap-0">
+              <ListItemIcon className="shrink"><MdDescription className="text-2xl"/></ListItemIcon>
+              <ListItemText className="basis-1/2" primary="Description"/>
+              <ListItemText className="basis-1/2" primary="Something"/>
+            </ListItem>
+            <ListItem className="flex gap-0">
+              <ListItemIcon className="shrink"><AiOutlineBarcode className="text-2xl"/></ListItemIcon>
+              <ListItemText className="basis-1/2" primary="Barcode"/>
+              <ListItemText className="basis-1/2" primary="1872083740192"/>
+            </ListItem>
+            <ListItem className="flex gap-0">
+              <ListItemIcon className="shrink"><SiCoderwall className="text-2xl"/></ListItemIcon>
+              <ListItemText className="basis-1/2" primary="Sequence code"/>
+              <ListItemText className="basis-1/2" primary="1234"/>
+            </ListItem>
+            <ListItem className="flex gap-0">
+              <ListItemIcon className="shrink"><GiPriceTag className="text-2xl"/></ListItemIcon>
+              <ListItemText className="basis-1/2" primary="Price"/>
+              <ListItemText className="basis-1/2" primary="R$ 16,45"/>
+            </ListItem>
+          </List>
+        </DialogContent>
+        <DialogActions>
+          <Button className="float-right" onClick={this.props.handleClose}>Close</Button>
+        </DialogActions>
       </Dialog>
     )
   }
