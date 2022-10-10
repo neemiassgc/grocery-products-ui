28b0071e1d3e0f766db97f798d8fb6507797bade
Neemias Santos <neemiassgc@outlook.com>
2022-10-10 03:58:43 +0100

    Take actions from the ModalView component and forward one of them to the SearchBar component

--- pages/index.jsx
+++ pages/index.jsx
@@ -5,6 +5,7 @@ import Head from "next/head"
 import Box from "@mui/material/Box"
 import InputAdornment from '@mui/material/InputAdornment';
 import { BiBarcodeReader } from "react-icons/bi"
+import ModalView from "../components/viewmodal"
 
 function Header() {
   return (
@@ -13,7 +14,7 @@ function Header() {
   )
 }
 
-function SearchBar() {
+function SearchBar(props) {
   return (
     <Box>
       <TextField
@@ -26,6 +27,7 @@ function SearchBar() {
           endAdornment: <InputAdornment position="end">
             <BiBarcodeReader
               className="text-2xl hover:text-black active:text-red-600 hover:cursor-pointer active:cursor-default"
+              onClick={props.handleOpenModal}
             />
           </InputAdornment>
         }}
@@ -37,6 +39,10 @@ function SearchBar() {
 export default class App extends Component {
   constructor(props) {
     super(props);
+  }
+
+  handleOpenModal() {
+    this.actions.openModal()
   }
 
   render() {
@@ -53,10 +59,11 @@ export default class App extends Component {
         <Header/>
         <div className="mt-3 sm:mt-8 flex justify-center">
           <div className="basis-full md:basis-11/12 h-[38rem] shadow-none md:shadow-2xl p-3 md:p-5 border-0 md:border rounded-md flex flex-col gap-2">
-            <SearchBar/>
+            <SearchBar handleOpenModal={this.handleOpenModal.bind(this)}/>
             <DataTable/>
           </div>
         </div>
+        <ModalView modalActions={actions => this.actions = actions} />
       </>
     )
   }
