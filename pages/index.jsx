8aec883cae864c209de2ab8f6bd0436af67000e1
Neemias Santos <neemiassgc@outlook.com>
2022-10-10 18:42:24 +0100

    Undo the Header component and put its content in the App component

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,13 +4,6 @@ import DataTable from "../modules/components/datatable"
 import ModalView from "../modules/components/viewmodal"
 import SearchBar from "../modules/components/searchbar"
 
-function Header() {
-  return (
-    <header className="w-full bg-blue-500 flex p-3">
-    </header>
-  )
-}
-
 export default class App extends Component {
   constructor(props) {
     super(props);
@@ -31,7 +24,7 @@ export default class App extends Component {
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" 
           />
         </Head>
-        <Header/>
+        <header className="w-full bg-blue-500 flex p-3" />
         <div className="mt-3 sm:mt-8 flex justify-center">
           <div className="basis-full md:basis-11/12 h-[38rem] shadow-none md:shadow-2xl p-3 md:p-5 border-0 md:border rounded-md flex flex-col gap-2">
             <SearchBar handleOpenModal={this.handleOpenModal.bind(this)}/>
