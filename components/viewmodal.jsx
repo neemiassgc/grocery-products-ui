7295593ac35d1ba1e73c2f4d0a1c480ced0599f3
Neemias Santos <neemiassgc@outlook.com>
2022-10-10 03:55:55 +0100

    Improve sketch with a loading screen and some actions

--- components/viewmodal.jsx
+++ components/viewmodal.jsx
@@ -8,53 +8,80 @@ import { MdDescription } from "react-icons/md"
 import { AiOutlineBarcode } from "react-icons/ai"
 import { SiCoderwall } from "react-icons/si"
 import { GiPriceTag } from "react-icons/gi"
+import CircularProgress from '@mui/material/CircularProgress';
+import Backdrop from '@mui/material/Backdrop';
 
 class DialogView extends Component {
 
   constructor(props) {
     super(props)
+
+    this.state = {
+      isLoading: false,
+      open: false,
+    }
+
+    props.modalActions({
+      openModal: this.openModal.bind(this),
+      closeModal: this.closeModal.bind(this),
+    })
+  }
+
+
+  openModal() {
+    this.setState({ open: true })
+  }
+
+  closeModal() {
+    this.setState({ open: false })
   }
 
   render() {
     return (
-      <Dialog
-        fullWidth={true}
-        maxWidth={"sm"}
-        open={this.props.open}
-      >
-        <DialogTitle>
-          <Alert severity="success" variant="filled">
-            <AlertTitle>Product is already exist!</AlertTitle>
-          </Alert>
-        </DialogTitle>
-        <DialogContent dividers={true}>
-          <List>
-            <ListItem className="flex gap-0">
-              <ListItemIcon className="shrink"><MdDescription className="text-2xl"/></ListItemIcon>
-              <ListItemText className="basis-1/2" primary="Description"/>
-              <ListItemText className="basis-1/2" primary="Something"/>
-            </ListItem>
-            <ListItem className="flex gap-0">
-              <ListItemIcon className="shrink"><AiOutlineBarcode className="text-2xl"/></ListItemIcon>
-              <ListItemText className="basis-1/2" primary="Barcode"/>
-              <ListItemText className="basis-1/2" primary="1872083740192"/>
-            </ListItem>
-            <ListItem className="flex gap-0">
-              <ListItemIcon className="shrink"><SiCoderwall className="text-2xl"/></ListItemIcon>
-              <ListItemText className="basis-1/2" primary="Sequence code"/>
-              <ListItemText className="basis-1/2" primary="1234"/>
-            </ListItem>
-            <ListItem className="flex gap-0">
-              <ListItemIcon className="shrink"><GiPriceTag className="text-2xl"/></ListItemIcon>
-              <ListItemText className="basis-1/2" primary="Price"/>
-              <ListItemText className="basis-1/2" primary="R$ 16,45"/>
-            </ListItem>
-          </List>
-        </DialogContent>
-        <DialogActions>
-          <Button className="float-right" onClick={this.props.handleClose}>Close</Button>
-        </DialogActions>
-      </Dialog>
+      <>
+        <Backdrop className="z-10" open={this.state.isLoading}>
+          <CircularProgress/>
+        </Backdrop>
+        <Dialog
+          fullWidth={true}
+          maxWidth={"sm"}
+          open={this.state.open}
+          className="bg-transparent"
+        >
+          <DialogTitle>
+            <Alert severity="error" variant="filled">
+              <AlertTitle>Product is already exist!</AlertTitle>
+            </Alert>
+          </DialogTitle>
+          <DialogContent dividers={true}>
+            <List>
+              <ListItem className="flex gap-0">
+                <ListItemIcon className="shrink"><MdDescription className="text-2xl"/></ListItemIcon>
+                <ListItemText className="basis-1/2" primary="Description"/>
+                <ListItemText className="basis-1/2" primary="Something"/>
+              </ListItem>
+              <ListItem className="flex gap-0">
+                <ListItemIcon className="shrink"><AiOutlineBarcode className="text-2xl"/></ListItemIcon>
+                <ListItemText className="basis-1/2" primary="Barcode"/>
+                <ListItemText className="basis-1/2" primary="1872083740192"/>
+              </ListItem>
+              <ListItem className="flex gap-0">
+                <ListItemIcon className="shrink"><SiCoderwall className="text-2xl"/></ListItemIcon>
+                <ListItemText className="basis-1/2" primary="Sequence code"/>
+                <ListItemText className="basis-1/2" primary="1234"/>
+              </ListItem>
+              <ListItem className="flex gap-0">
+                <ListItemIcon className="shrink"><GiPriceTag className="text-2xl"/></ListItemIcon>
+                <ListItemText className="basis-1/2" primary="Price"/>
+                <ListItemText className="basis-1/2" primary="R$ 16,45"/>
+              </ListItem>
+            </List>
+          </DialogContent>
+          <DialogActions>
+            <Button className="float-right" onClick={this.closeModal.bind(this)}>Close</Button>
+          </DialogActions>
+        </Dialog>
+      </>
     )
   }
 }
