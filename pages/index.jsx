4a67e568c2a64f3800ee1167023bf8cd01de8ac9
Neemias Santos <neemiassgc@outlook.com>
2022-10-20 18:33:28 +0100

    style: Make the table content overlap the horizon at desktop screens and remove the top-margin at mobile screens

--- pages/index.jsx
+++ pages/index.jsx
@@ -26,9 +26,9 @@ export default class App extends Component {
           <title>Saveg Local Market</title>
           <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
         </Head>
-        <header className="w-full bg-blue-500 flex p-3" />
-        <div className="mt-3 sm:mt-8 flex justify-center">
-          <div className="basis-full md:basis-11/12 h-[38rem] shadow-none md:shadow-2xl p-3 md:p-5 border-0 md:border rounded-md flex flex-col gap-2">
+        <header className="w-full bg-blue-500 flex h-0 md:h-72"/>
+        <div className=" mt-3 md:-mt-52 flex justify-center">
+          <div className="basis-full md:basis-11/12 h-[38rem] shadow-none md:shadow-2xl p-3 md:p-5 border-0 md:border rounded-md flex flex-col gap-2 bg-white">
             <SearchBar
               searchByBarcode={this.handleSearchByBarcode.bind(this)}
               actions={actions => this.actions.searchBar = actions}
