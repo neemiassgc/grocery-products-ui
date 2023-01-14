086ee694cd46c5b03612a846648dfc9b32e977c0
Neemias Santos <neemiassgc@outlook.com>
2023-01-14 11:48:03 +0000

    refactor: Extract a new component from AlertModal named ContentData

--- modules/components/alert-modal.jsx
+++ modules/components/alert-modal.jsx
@@ -130,45 +130,8 @@ class AlertModal extends Component {
             this.state.content.status === 404 ?
             <Box className="flex justify-center p-2">
               <BiError className="text-9xl text-black"/>
-            </Box> :
-            <List>
-              <ListItem className="flex flex-col md:flex-row justify-center gap-0 md:gap-5 mb-2">
-                <Box className="basis-1/3">
-                  <MdDescription className="text-2xl mr-3 inline-block align-middle"/>
-                  <ListItemText className="inline-block align-middle" primary="Description:"/>
-                </Box>
-                <Box className="basis-1/3">
-                  <ListItemText primary={this.state.content.body?.description}></ListItemText>
-                </Box>
-              </ListItem>
-              <ListItem className="flex flex-col md:flex-row justify-center gap-0 md:gap-5 mb-2">
-                <Box className="basis-1/3">
-                  <AiOutlineBarcode className="text-2xl mr-3 inline-block align-middle"/>
-                  <ListItemText className="inline-block align-middle" primary="Barcode"/>
-                </Box>
-                <Box className="basis-1/3">
-                  <ListItemText primary={this.state.content.body?.barcode}></ListItemText>
-                </Box>
-              </ListItem>
-              <ListItem className="flex flex-col md:flex-row justify-center gap-0 md:gap-5 mb-2">
-                <Box className="basis-1/3">
-                  <SiCoderwall className="text-2xl mr-3 inline-block align-middle"/>
-                  <ListItemText className="inline-block align-middle" primary="Sequence code"/>
-                </Box>
-                <Box className="basis-1/3">
-                  <ListItemText primary={this.state.content.body?.sequenceCode}></ListItemText>
-                </Box>
-              </ListItem>
-              <ListItem className="flex flex-col md:flex-row justify-center gap-0 md:gap-5">
-                <Box className="basis-1/3">
-                  <GiPriceTag className="text-2xl mr-3 inline-block align-middle"/>
-                  <ListItemText className="inline-block align-middle" primary="Current price"/>
-                </Box>
-                <Box className="basis-1/3">
-                  <ListItemText primary={priceFormatter(this.state.content.body?.currentPrice)}></ListItemText>
-                </Box>
-              </ListItem>
-            </List>
+            </Box> : <ContentData body={this.state.content.body}/>
+            
           }
           </DialogContent>
           <DialogActions>
@@ -178,6 +141,49 @@ class AlertModal extends Component {
       </>
     )
   }
+}
+
+function ContentData(props) {
+  return (
+    <List>
+      <ListItem className="flex flex-col md:flex-row justify-center gap-0 md:gap-5 mb-2">
+        <Box className="basis-1/3">
+          <MdDescription className="text-2xl mr-3 inline-block align-middle"/>
+          <ListItemText className="inline-block align-middle" primary="Description:"/>
+        </Box>
+        <Box className="basis-1/3">
+          <ListItemText primary={props.body?.description}></ListItemText>
+        </Box>
+      </ListItem>
+      <ListItem className="flex flex-col md:flex-row justify-center gap-0 md:gap-5 mb-2">
+        <Box className="basis-1/3">
+          <AiOutlineBarcode className="text-2xl mr-3 inline-block align-middle"/>
+          <ListItemText className="inline-block align-middle" primary="Barcode"/>
+        </Box>
+        <Box className="basis-1/3">
+          <ListItemText primary={props.body?.barcode}></ListItemText>
+        </Box>
+      </ListItem>
+      <ListItem className="flex flex-col md:flex-row justify-center gap-0 md:gap-5 mb-2">
+        <Box className="basis-1/3">
+          <SiCoderwall className="text-2xl mr-3 inline-block align-middle"/>
+          <ListItemText className="inline-block align-middle" primary="Sequence code"/>
+        </Box>
+        <Box className="basis-1/3">
+          <ListItemText primary={props.body?.sequenceCode}></ListItemText>
+        </Box>
+      </ListItem>
+      <ListItem className="flex flex-col md:flex-row justify-center gap-0 md:gap-5">
+        <Box className="basis-1/3">
+          <GiPriceTag className="text-2xl mr-3 inline-block align-middle"/>
+          <ListItemText className="inline-block align-middle" primary="Current price"/>
+        </Box>
+        <Box className="basis-1/3">
+          <ListItemText primary={priceFormatter(props.body?.currentPrice)}></ListItemText>
+        </Box>
+      </ListItem>
+    </List>
+  )
 }
 
 export default AlertModal
