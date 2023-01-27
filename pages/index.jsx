03692e705928799b26c8dfa5d2e57965e9856138
Neemias Santos <neemiassgc@outlook.com>
2023-01-26 23:58:50 -0300

    refactor: Turn the App component into a React function component

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,4 +1,4 @@
-import { Component } from "react"
+import { useEffect, useState } from "react"
 import Head from "next/head"
 import DataTable from "../modules/components/datatable"
 import AlertModal from "../modules/components/alert-modal"
@@ -6,62 +6,37 @@ import SearchBar from "../modules/components/searchbar"
 import ScannerModal from "../modules/components/scanner-modal"
 import { isPossibleToScanForBarcodes } from "../modules/utils"
 
-export default class App extends Component {
-  constructor(props) {
-    super(props);
+function App() {
+  const [scannerModalAvailable, setScannerModalAvailable] = useState(false);
 
-    this.actions = {}
+  useEffect(() => {
+    if (isPossibleToScanForBarcodes) setScannerModalAvailable(true)
+  })
 
-    this.state = {
-      scannerModalAvailable: false
-    }
-  }
+  const [actions, setActions] = useState({})
 
-  componentDidMount() {
-    if (isPossibleToScanForBarcodes)
-      this.setScannerModalAvailable(true)
-  }
-
-  setScannerModalAvailable(bool) {
-    this.setState({ scannerModalAvailable: bool })
-  }
-
-  handleSearchByBarcode(barcode) {
-    this.actions.alertModal.searchByBarcode(barcode)
-  }
-
-  handleShowError(violations) {
-    this.actions.searchBar.showError(violations);
-  }
-  
-  handleShowScannerModal() {
-    this.actions.openScannerModal();
-  }
-
-  render() {
-    return(
-      <>
-        <Head>
-          <title>Grocery products ui</title>
-          <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
-        </Head>
-        <header className="w-full bg-blue-500 flex h-0 md:h-72"/>
-        <div className=" mt-3 md:-mt-52 flex justify-center">
-          <div className="basis-full md:basis-11/12 h-[38rem] shadow-none md:shadow-2xl p-3 md:p-5 border-0 md:border rounded-md flex flex-col gap-2 bg-white">
-            <SearchBar
-              searchByBarcode={this.handleSearchByBarcode.bind(this)}
-              openScannerModal={this.handleShowScannerModal.bind(this)}
-              actions={actions => this.actions.searchBar = actions}
-            />
-            <DataTable/>
-          </div>
+  return <>
+      <Head>
+        <title>Grocery products ui</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
+      </Head>
+      <header className="w-full bg-blue-500 flex h-0 md:h-72"/>
+      <div className=" mt-3 md:-mt-52 flex justify-center">
+        <div className="basis-full md:basis-11/12 h-[38rem] shadow-none md:shadow-2xl p-3 md:p-5 border-0 md:border rounded-md flex flex-col gap-2 bg-white">
+          <SearchBar
+            searchByBarcode={actions?.alertModal?.searchByBarcode}
+            openScannerModal={actions?.openScannerModal}
+            actions={methods => setActions({...actions, ...methods})}
+          />
+          <DataTable/>
         </div>
-        <AlertModal showFieldError={this.handleShowError.bind(this)} actions={actions => this.actions.alertModal = actions} />
-        {
-          this.state.scannerModalAvailable &&
-          <ScannerModal searchByBarcode={this.handleSearchByBarcode.bind(this)} actions={({ openModal }) => this.actions.openScannerModal = openModal}/>
-        }
-      </>
-    )
-  }
+      </div>
+      <AlertModal showFieldError={actions?.searchbar?.showError} actions={methods => setActions({...actions, ...methods})} />
+      {
+        scannerModalAvailable &&
+        <ScannerModal searchByBarcode={actions?.alertModal?.searchByBarcode} actions={({ openModal }) => setActions({...actions, openModal})}/>
+      }
+  </>
 }
+
+export default App
