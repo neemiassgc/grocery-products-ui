dc284dc30822fe6c535e2244c532842bf45d97a5
Neemias Santos <neemiassgc@outlook.com>
2023-01-30 20:21:06 -0300

    refactor: Extract a new component named ListDataBlock from the ContentData component

--- modules/components/alert-modal.jsx
+++ modules/components/alert-modal.jsx
@@ -190,45 +190,28 @@ function ErrorBoard({ netError }) {
 }
 
 function ContentData(props) {
+  const iconStyleClasses = "text-2xl mr-3 inline-block align-middle";
   return (
     <List>
-      <ListItem className="flex flex-col md:flex-row justify-center gap-0 md:gap-5 mb-2">
-        <Box className="basis-1/3">
-          <MdDescription className="text-2xl mr-3 inline-block align-middle"/>
-          <ListItemText className="inline-block align-middle" primary="Description:"/>
-        </Box>
-        <Box className="basis-1/3">
-          <ListItemText primary={props.body?.description}></ListItemText>
-        </Box>
-      </ListItem>
-      <ListItem className="flex flex-col md:flex-row justify-center gap-0 md:gap-5 mb-2">
-        <Box className="basis-1/3">
-          <AiOutlineBarcode className="text-2xl mr-3 inline-block align-middle"/>
-          <ListItemText className="inline-block align-middle" primary="Barcode"/>
-        </Box>
-        <Box className="basis-1/3">
-          <ListItemText primary={props.body?.barcode}></ListItemText>
-        </Box>
-      </ListItem>
-      <ListItem className="flex flex-col md:flex-row justify-center gap-0 md:gap-5 mb-2">
-        <Box className="basis-1/3">
-          <SiCoderwall className="text-2xl mr-3 inline-block align-middle"/>
-          <ListItemText className="inline-block align-middle" primary="Sequence code"/>
-        </Box>
-        <Box className="basis-1/3">
-          <ListItemText primary={props.body?.sequenceCode}></ListItemText>
-        </Box>
-      </ListItem>
-      <ListItem className="flex flex-col md:flex-row justify-center gap-0 md:gap-5">
-        <Box className="basis-1/3">
-          <GiPriceTag className="text-2xl mr-3 inline-block align-middle"/>
-          <ListItemText className="inline-block align-middle" primary="Current price"/>
-        </Box>
-        <Box className="basis-1/3">
-          <ListItemText primary={priceFormatter(props.body?.currentPrice)}></ListItemText>
-        </Box>
-      </ListItem>
+      <ListDataBlock value={props.body?.description} name="Description:" Icon={() => <MdDescription className={iconStyleClasses}/>}/>
+      <ListDataBlock value={props.body?.barcode} name="Barcode" Icon={() => <AiOutlineBarcode className={iconStyleClasses}/>}/>
+      <ListDataBlock value={props.body?.sequenceCode} name="Sequence Code" Icon={() => <SiCoderwall className={iconStyleClasses}/>}/>
+      <ListDataBlock value={priceFormatter(props.body?.currentPrice)} name="Current Price" Icon={() => <GiPriceTag className={iconStyleClasses}/>}/>
     </List>
+  )
+}
+
+function ListDataBlock({ Icon, value, name}) {
+  return (
+    <ListItem className="flex flex-col md:flex-row justify-center gap-0 md:gap-5 mb-2">
+      <Box className="basis-1/3">
+        <Icon/>
+        <ListItemText className="inline-block align-middle" primary={name}/>
+      </Box>
+      <Box className="basis-1/3">
+        <ListItemText primary={value}></ListItemText>
+      </Box>
+    </ListItem>
   )
 }
 
