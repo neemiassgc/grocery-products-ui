baa3303c333493b9ed834a8ea35c77d39cec69e2
Neemias Santos <neemiassgc@outlook.com>
2023-01-31 09:54:13 -0300

    refactor: Use function component instead of class component

--- modules/components/searchbar.jsx
+++ modules/components/searchbar.jsx
@@ -5,49 +5,40 @@ import FormControl from "@mui/material/FormControl";
 import OutlinedInput from "@mui/material/OutlinedInput";
 import FormHelperText from "@mui/material/FormHelperText";
 import { BiBarcodeReader } from "react-icons/bi"
-import { Component } from "react"
 
-class SearchBar extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <Box>
-        <FormControl
-          variant="outlined" className="w-full"
-          error={this.props.error}
-          size="small"
-        >
-          <InputLabel htmlFor="my-input">Search by barcode</InputLabel>
-          <OutlinedInput
-            id="my-input"
-            className="w-full"
-            label="Search by barcode"
-            endAdornment={
-              this.props.scannerButtonAvailable &&
-              <InputAdornment position="end">
-                <BiBarcodeReader
-                  className="text-2xl hover:text-black active:text-red-600 hover:cursor-pointer active:cursor-default"
-                  // onClick={this.props.openScannerModal}
-                />
-              </InputAdornment>
-            }
-            onKeyUp={this.props.onKeyUp}
-            onChange={this.props.onChange}
-            value={this.props.value}
-          />
-          {
-            this.props.error &&
-            <FormHelperText className="font-medium">
-              {this.props.helperTextContent}
-            </FormHelperText>
+export default function SearchBar(props) {
+  return (
+    <Box>
+      <FormControl
+        variant="outlined" className="w-full"
+        error={props.error}
+        size="small"
+      >
+        <InputLabel htmlFor="my-input">Search by barcode</InputLabel>
+        <OutlinedInput
+          id="my-input"
+          className="w-full"
+          label="Search by barcode"
+          endAdornment={
+            props.scannerButtonAvailable &&
+            <InputAdornment position="end">
+              <BiBarcodeReader
+                className="text-2xl hover:text-black active:text-red-600 hover:cursor-pointer active:cursor-default"
+                // onClick={props.openScannerModal}
+              />
+            </InputAdornment>
           }
-        </FormControl>
-      </Box>
-    )
-  }
+          onKeyUp={props.onKeyUp}
+          onChange={props.onChange}
+          value={props.value}
+        />
+        {
+          props.error &&
+          <FormHelperText className="font-medium">
+            {props.helperTextContent}
+          </FormHelperText>
+        }
+      </FormControl>
+    </Box>
+  )
 }
-
-export default SearchBar;
