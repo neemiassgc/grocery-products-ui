8ed395d88544cbbd125119b197de200f3d74f1a4
Neemias Santos <neemiassgc@outlook.com>
2023-01-31 09:51:46 -0300

    refactor: Lift state up from SearchBar component to its parent component called SearchableContainer

--- modules/components/searchable-container.jsx
+++ modules/components/searchable-container.jsx
@@ -1,15 +1,47 @@
 import AlertModal from "./alert-modal"
 import SearchBar from "./searchbar"
 import ScannerModal from "./scanner-modal"
-import { isPossibleToScanForBarcodes } from "../utils"
+import { isPossibleToScanForBarcodes, isANumber, isZero } from "../utils"
 import { useEffect, useState, useRef } from "react"
 
 function SearchableContainer() {
   const [scannerModalAvailable, setScannerModalAvailable] = useState(false);
+  const [searchBarState, setSearchBarState] = useState({
+    value: "",
+    error: false,
+    helperTextContent: ""
+  })
 
   useEffect(() => {
-    if (isPossibleToScanForBarcodes) setScannerModalAvailable(true)
+    if (isPossibleToScanForBarcodes()) setScannerModalAvailable(true)
   }, [])
+
+  const handleError = violations => {
+    const errorFeedback = violations
+      .map(violation => <p>{violation.violationMessage}</p>)
+
+    setSearchBarState({
+      ...searchBarState, helperTextContent: errorFeedback, error: true,
+    });
+  }
+
+  const handleChange = ({ target: { value } }) => {
+    if (searchBarState.error)
+      setSearchBarState({...searchBarState, error: false, helperTextContent: ""})
+
+    if (value.length <= 13 && isANumber(value))
+      setSearchBarState({...searchBarState, value});
+  }
+
+  const handleKeyUp = ({ key }) => {
+    if (key === "Enter") {
+      if (isZero(searchBarState.value.length)) {
+        handleError([{ violationMessage: "barcode cannot be empty" }])
+        return;
+      }
+      // this.props.findByBarcodeAndOpenAlertModal(searchBarState.input.value)
+    }
+  }
 
   const searchBarRef = useRef(null);
   const alertModalRef = useRef(null);
@@ -22,8 +54,13 @@ function SearchableContainer() {
       <ScannerModal findByBarcodeAndOpenAlertModal={alertModalRef.current?.findByBarcodeAndLoadData} actionRef={scannerModalRef}/>
     }
     <SearchBar
-      findByBarcodeAndOpenAlertModal={alertModalRef.current?.findByBarcodeAndLoadData}
-      openScannerModal={scannerModalRef.current?.openScannerModal}
+      error={searchBarState.error}
+      value={searchBarState.value}
+      helperTextContent={searchBarState.helperTextContent}
+      scannerButtonAvailable={scannerModalAvailable}
+      onKeyUp={handleKeyUp}
+      onChange={handleChange}
+      // openScannerModal={scannerModalRef.current?.openScannerModal}
       actionRef={searchBarRef}
     />
   </>)
--- modules/components/searchbar.jsx
+++ modules/components/searchbar.jsx
@@ -6,86 +6,10 @@ import OutlinedInput from "@mui/material/OutlinedInput";
 import FormHelperText from "@mui/material/FormHelperText";
 import { BiBarcodeReader } from "react-icons/bi"
 import { Component } from "react"
-import { isANumber, isPossibleToScanForBarcodes, isZero } from '../utils'
 
 class SearchBar extends Component {
   constructor(props) {
     super(props);
-
-    this.state = {
-      input: {
-        error: false,
-        value: "",
-        errorContent: "",
-      },
-      scannerButtonAvailable: false,
-    }
-  }
-
-  componentDidMount() {
-    if (isPossibleToScanForBarcodes())
-      this.setScannerButtonAvailable(true)
-
-    this.props.actionRef.current = {
-      showError: this.handleError.bind(this)
-    }
-  }
-
-  handleError(violations) {
-    const errorFeedback = violations
-      .map(violation => <p>{violation.violationMessage}</p>)
-
-    this.setInputErrorContent(errorFeedback);
-    this.setInputError(true);
-  }
-
-  handleTextField({ target: { value } }) {
-    if (this.state.input.error) {
-      this.setInputErrorContent("");
-      this.setInputError(false)
-    }
-
-    if (value.length <= 13 && isANumber(value)) this.setInputValue(value)
-  }
-
-  setInputError(error) {
-    this.setInputState({ error });
-  }
-
-  setInputErrorContent(errorContent) {
-    this.setInputState({ errorContent });
-  }
-
-  setInputValue(value) {
-    this.setInputState({ value });
-  }
-
-  setInputState(objectChunk) {
-    this.setState(prevState => {
-      const [key] = Object.keys(objectChunk)
-      return {
-        input: {
-          ...prevState.input,
-          [key]: objectChunk[key]
-        }
-      }
-    })
-  }
-
-  setScannerButtonAvailable(bool) {
-    this.setState({ scannerButtonAvailable: bool })
-  }
-
-  handleKeyUp({ key }) {
-    if (key === "Enter") {
-      if (isZero(this.state.input.value.length)) {
-        this.handleError([{
-            violationMessage: "barcode cannot be empty"
-        }])
-        return;
-      }
-      this.props.findByBarcodeAndOpenAlertModal(this.state.input.value)
-    }
   }
 
   render() {
@@ -93,7 +17,7 @@ class SearchBar extends Component {
       <Box>
         <FormControl
           variant="outlined" className="w-full"
-          error={this.state.input.error}
+          error={this.props.error}
           size="small"
         >
           <InputLabel htmlFor="my-input">Search by barcode</InputLabel>
@@ -102,22 +26,22 @@ class SearchBar extends Component {
             className="w-full"
             label="Search by barcode"
             endAdornment={
-              this.state.scannerButtonAvailable &&
+              this.props.scannerButtonAvailable &&
               <InputAdornment position="end">
                 <BiBarcodeReader
                   className="text-2xl hover:text-black active:text-red-600 hover:cursor-pointer active:cursor-default"
-                  onClick={this.props.openScannerModal}
+                  // onClick={this.props.openScannerModal}
                 />
               </InputAdornment>
             }
-            onKeyUp={this.handleKeyUp.bind(this)}
-            onChange={this.handleTextField.bind(this)}
-            value={this.state.input.value}
+            onKeyUp={this.props.onKeyUp}
+            onChange={this.props.onChange}
+            value={this.props.value}
           />
           {
-            this.state.input.error &&
+            this.props.error &&
             <FormHelperText className="font-medium">
-              {this.state.input.errorContent}
+              {this.props.helperTextContent}
             </FormHelperText>
           }
         </FormControl>
