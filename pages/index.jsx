8e12406b7625b57c49f09aa24e3b2ad38db63f20
Neemias Santos <neemiassgc@outlook.com>
2023-09-20 06:29:44 +0100

    new: Implement the first version of the authentication and authorization flows using Oauth2

--- pages/index.jsx
+++ pages/index.jsx
@@ -36,84 +36,82 @@ function PageLoading({children}) {
   const [error, setError] = useState(false);
   const [errorMessage, setErrorMessage] = useState("");
 
-  useEffect(() => {
-    const accessToken = storage.getAccessToken();
 
-    if (!accessToken) {
-      if (isThereErrorParam()) {
-        const warningsMap = {
-          access_denied: "Access Denied",
-          invalid_scope: "Invalid scopes"
-        }
-        const errorValue = extractErrorFromUri();
-        setErrorMessage(warningsMap[errorValue]);
-        setError(true)
-        setLoading(false)
-      }
-      else if (isNotThereCodeParam()) {
-        const stateUUID = createUUID();
-        storage.saveUUID(stateUUID);
-        
-        const authUrl = storage.getRootAuthUrl();
-        const path = "/realms/security/protocol/openid-connect/auth";
-        const params = "?response_type=code&client_id=grocerystoreapp&scope=grocerystoreapp&state="+stateUUID+"&=redirect_uri="+window.location.href;
-        window.location.href = authUrl+path+params
-      }
-      else {
-        stateValidationForCsrfProtection()
-        .then(() => {
-          const code = extractAuthCodeFromUri();
-          requestAccessTokenUsingCode(code)
-            .then(jsonObj => {
-              storage.saveAccessToken(jsonObj.access_token);
-              storage.saveRefreshToken(jsonObj.refresh_token);
-              hitResourceServer()
-                .then(() => setLoading(false))
-                .catch(error => {
-                  setError(true);
-                  setLoading(false);
-                  setErrorMessage(error);
+  const setErrorPageWithMessage = errorMessage => {
+    setError(true);
+    setLoading(false);
+    setErrorMessage(errorMessage);
+  }
+
+  const showErrorScreenWithData = errorObj => {
+    const {headers} = errorObj;
+    if (wwwAuthenticateHeaderDoesNotExit(headers.entries())) return;
+    const [typeOfError, errorMessage] = extractErrorDataFromWwwAuthenticateHeader(headers.entries())
+    setErrorPageWithMessage(`${typeOfError}: ${errorMessage}`);
+  }
+
+  useEffect(() => {
+    isThereValidAccessToken()
+      .then(() => {
+        hitResourceServer()
+          .then(() => setLoading(false))
+          .catch(errorObj => {
+            if (errorObj instanceof TypeError) {
+              setErrorPageWithMessage(`${errorObj.name}: ${error.message}`)
+              return;
+            }
+            if (errorObj.status === 401) {
+              requestAccessTokenUsingRefreshToken()
+                .then(response => {
+                  storage.saveAccessToken(response.access_token);
+                  storage.saveRefreshToken(response.refresh_token);
+                  window.location.reload();
                 })
-            })
-            .catch(errorObj => {
-              console.log(errorObj)
-              setError(true);
-              setLoading(false);
-              setErrorMessage(errorObj.error_description);
-            })
-        })
-        .catch(errorMessage => {
-          setError(true);
-          setLoading(false);
-          setErrorMessage(errorMessage);
-        })
-      }
-    }
-    else {
-      hitResourceServer()
-        .then(() => setLoading(false))
-        .catch((err) => {
-          if (err instanceof TypeError) {
-            setError(true);
-            setLoading(false);
-            setErrorMessage(`${err.name}: ${err.message}`);
-            return;
+                .catch(resp => {
+                  storage.removeAccessToken();
+                  storage.removeRefreshToken();
+                  showErrorScreenWithData(resp)
+                })
+              return;
+            }
+            showErrorScreenWithData(errorObj)
+          })
+      })
+      .catch(() => {     
+         if (isThereErrorQueryParam()) {
+          const warningsMap = {
+            access_denied: "Access Denied",
+            invalid_scope: "Invalid scopes"
           }
-          requestAccessTokenUsingRefreshToken()
-            .then(response => {
-              console.log("saving refresh token")
-              storage.saveAccessToken(response.access_token);
-              storage.saveRefreshToken(response.refresh_token);
-              window.location.reload();
+          const errorValue = extractErrorFromUri();
+          setErrorPageWithMessage(warningsMap[errorValue])
+         }
+         else if (isNotThereCodeQueryParam()) {
+          const stateUUID = createUUID();
+          storage.saveUUID(stateUUID);
+          
+          const authUrl = storage.getRootAuthUrl();
+          const path = "/realms/security/protocol/openid-connect/auth";
+          const params = "?response_type=code&client_id=grocerystoreapp&scope=grocerystoreapp&state="+stateUUID+"&=redirect_uri="+window.location.href;
+          window.location.href = authUrl+path+params
+         }
+         else {
+          verifyStateQueryParam()
+            .then(() => {
+              const code = extractAuthCodeFromUri();
+              requestAccessTokenUsingCode(code)
+                .then(jsonObj => {
+                  storage.saveAccessToken(jsonObj.access_token);
+                  storage.saveRefreshToken(jsonObj.refresh_token);
+                  hitResourceServer()
+                    .then(() => setLoading(false))
+                    .catch(showErrorScreenWithData)
+                })
+                .catch(({error, error_description}) => setErrorPageWithMessage(`${error}: ${error_description}`));
             })
-            .catch(error => {
-              storage.saveAccessToken(null)
-              setError(true);
-              setLoading(false);
-              setErrorMessage(error);
-            })
+            .catch(() => setErrorPageWithMessage("It was not possible to validate integrity"))
+         }
         })
-    }
   }, [])
 
   const loader = <div className="w-screen h-screen flex justify-center items-center">
